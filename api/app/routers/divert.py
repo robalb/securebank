639cbc7838665dd4231374535477190c0363ec98
robalb <laptophp@ventafridda.com>
2022-06-17 23:32:24 +0200

    working on divert

--- api/app/routers/divert.py
+++ api/app/routers/divert.py
@@ -1,4 +1,17 @@
 from fastapi import APIRouter, HTTPException
 
-router = APIRouter(prefix='/api/divert')
+router = APIRouter(prefix='/api/transfer', tags=['transfer'])
+from fastapi.logger import logger
+from pydantic import BaseModel, Field, conint, UUID4
+from typing import Union
+from app.database import Cursor
+from app.procedures import procedure_change_balance, procedure_log_transfer, procedure_edit_account_info
 
+router = APIRouter(prefix='/api/divert', tags=['divert'])
+
+class Divert(BaseModel):
+  uid: UUID4 = Field(..., alias='id')
+
+@router.post("/")
+async def divert(d: Divert):
+  return d
