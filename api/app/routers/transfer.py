6c1676ecab8b01932804ee43a88c48cc6ec6cbde
robalb <alberto.ventafridda@gmail.com>
2022-06-17 15:49:40 +0200

    started work on transfer

--- api/app/routers/transfer.py
+++ api/app/routers/transfer.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
-router = APIRouter(prefix='/api/transfer')
+router = APIRouter(prefix='/api/transfer', tags=['transfer'])
+from fastapi.logger import logger
+from pydantic import BaseModel, constr
+from typing import Union
 
