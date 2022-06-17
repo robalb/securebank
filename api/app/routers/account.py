c67785da6f0c9ae32545745fd114f549cb4edd07
robalb <alberto.ventafridda@gmail.com>
2022-06-17 15:01:02 +0200

    completed account route

--- api/app/routers/account.py
+++ api/app/routers/account.py
@@ -8,7 +8,7 @@ from app.database import Cursor
 from argon2 import PasswordHasher
 import uuid
 
-router = APIRouter(prefix='/api/account')
+router = APIRouter(prefix='/api/account', tags=['account'])
 
 
 @router.get("/")
@@ -115,7 +115,6 @@ async def transfer_to_account(accountid: constr(min_length=20, max_length=20), a
 
 
 
-
 @router.get("/{accountid}")
 async def get_account_info(accountid: constr(min_length=20, max_length=20)):
     try:
@@ -125,7 +124,7 @@ async def get_account_info(accountid: constr(min_length=20, max_length=20)):
             account = cur.fetchone()
             #get transactions info
             cur.execute("""
-                    SELECT id, sender_id, receiver_id, UNIX_TIMESTAMP(time) as time, amount, description from transfers
+                    SELECT id, sender_id, receiver_id, UNIX_TIMESTAMP(time) AS time, amount, description FROM transfers
                     WHERE sender_id = ? or receiver_id = ?
                     ORDER BY time ASC LIMIT 500
                     """, (accountid, accountid))
@@ -138,6 +137,56 @@ async def get_account_info(accountid: constr(min_length=20, max_length=20)):
     responseHeaders = {"X-Sistema-Bancario": f"{account['name']};{account['surname']}"}
     return JSONResponse(content=responseJSON, headers=responseHeaders)
 
+
+class EditAccountInfo(BaseModel):
+    name: Union[str, None] = None
+    surname: Union[str, None] = None
+
+def procedure_edit_account_info(cur, accountid, name, surname):
+    try:
+        rowcount = 0
+        if name is not None:
+            cur.execute("UPDATE accounts SET name=? where id= ?", (name, accountid))
+            rowcount += cur.rowcount
+        if surname is not None:
+            cur.execute("UPDATE accounts SET surname=? where id= ?", (surname, accountid))
+            rowcount += cur.rowcount
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="transaction_failed")
+    if rowcount == 0:
+        raise HTTPException(status_code=400, detail="invalid_id_or_repeated_operation")
+
+@router.put("/{accountid}")
+async def edit_account_info(accountid: constr(min_length=20, max_length=20), newdata: EditAccountInfo):
+    if newdata.name is None or newdata.surname is None:
+        raise HTTPException(status_code=422, detail=[{"msg":"name and surname required"}])
+    with Cursor() as cur:
+        procedure_edit_account_info(cur, accountid, newdata.name, newdata.surname)
+    return {"success": True}
+
+
+@router.patch("/{accountid}")
+async def edit_account_info(accountid: constr(min_length=20, max_length=20), newdata: EditAccountInfo):
+    if newdata.name is None and newdata.surname is None:
+        raise HTTPException(status_code=422, detail=[{"msg":"name or surname required"}])
+    with Cursor() as cur:
+        procedure_edit_account_info(cur, accountid, newdata.name, newdata.surname)
+    return {"success": True}
+
+
+@router.head("/{accountid}")
+async def weird_head(accountid: constr(min_length=20, max_length=20)):
+    try:
+        with Cursor() as cur:
+            #get account info
+            cur.execute("SELECT name, surname, balance FROM accounts WHERE id=?", (accountid,))
+            account = cur.fetchone()
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="transaction_failed")
+    responseHeaders = {"X-Sistema-Bancario": f"{account['name']};{account['surname']}"}
+    return JSONResponse(headers=responseHeaders)
 
 
 
