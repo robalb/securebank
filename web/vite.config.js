1cdc5edb9b615e3f42c19e4c9a182d31b0de654f
robalb <alberto.ventafridda@gmail.com>
2022-06-20 03:54:44 +0200

    added docker-compose for both dev and prod, with relative configs

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,11 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 const root = resolve(__dirname, 'src/pages')
 const outDir = resolve(__dirname, 'dist')
+const publicDir = resolve(__dirname, 'public')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
   plugins: [svelte()],
+  publicDir: publicDir,
   build: {
     outDir,
     assetsDir: 'static',
@@ -16,7 +18,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
-        transfer: resolve(root, 'info', 'index.html'),
+        transfer: resolve(root, 'transfer', 'index.html'),
       }
     }
   },
